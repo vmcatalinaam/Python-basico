fff4db93946f3675c6b8e3e5032e8959e31f8951
Catalina Valderrama M <91352571+vmcatalinaam@users.noreply.github.com>
2023-02-08 18:28:10 -0500

    Add files via upload

---
+++ Funciones utiles.py
@@ -0,0 +1,24 @@
+# FUNCIONES UTILES
+
+# REDONDEO
+
+print(round(3.4567))
+
+# VALOR ABSOLUTO
+
+print(abs(-56))
+
+# CREAR LISTA
+
+lista  = [3,56,34,2,6,23,65,72,36]
+
+print(min(lista))
+print(max(lista))
+
+# ordernar lista
+
+print(sorted(lista))
+
+# suma total lista
+
+print(sum(lista))
