05e4228b9774a686a8cecb219d7853628e0c8f88
Catalina Valderrama M <91352571+vmcatalinaam@users.noreply.github.com>
2023-02-08 18:27:44 -0500

    Add files via upload

---
+++ Paquetes/llamado_funcion.py
@@ -0,0 +1,14 @@
+
+# libreria para saber is es par
+
+def esPar(numero_):
+    if numero_%2 == 0: #modulo %
+        #print('El numero es par')
+        return True
+    else:
+       # print('El numero es impar') 
+       return False
+    
+# libreria para saber suma
+def suma(num1,num2):
+    return num1 + num2
